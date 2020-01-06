f7791de969f15f6ff22e219ea688bfd0f38a18aa
codybreene <codybreene@gmail.com>
2020-01-06 13:42:06 -0800

    add entry js file

---
+++ frontend/merkle_money.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+// set up entry file
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    const root = document.getElementById('root');
+    // store const to go here
+    ReactDOM.render(<h1>Hello React is working sort of!</h1>, root)
+})
+
