d7e6ce21f3c58d4a01f39b322183ab4cfedaec3c
codybreene <codybreene@gmail.com>
2020-01-06 16:31:42 -0800

    configure store and add to window for testing purposes

--- frontend/merkle_money.jsx
+++ frontend/merkle_money.jsx
@@ -5,15 +5,18 @@ import {
     signup,
     logout
 } from './util/session_api_util'
+import configureStore from './store/store';
 
 // set up entry file
 
 document.addEventListener('DOMContentLoaded', ()=> {
     const root = document.getElementById('root');
-    // store const to go here
+    const store = configureStore;
     window.login = login;
     window.signup = signup;
     window.logout = logout;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     ReactDOM.render(<h1>Hello React is working sort of!</h1>, root)
 })
 
