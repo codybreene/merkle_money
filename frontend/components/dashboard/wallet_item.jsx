7433286501f3cf3633ef00b6028f08ad019c7949
codybreene <codybreene@gmail.com>
2020-02-18 22:47:19 -0800

    add wallets component

---
+++ frontend/components/dashboard/wallet_item.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const WalletItem = ({wallet, balance, logo}) => {
+  const name = logo.name
+  const img = logo.logo
+  const sym = wallet.currency.toUpperCase()
+  return (
+    <tr>
+      <td className="col-2-content">
+        <div className="col-2-content-inner">
+          <img className="curr-logo-splash" src={`${img}`} />
+          <div className="table-crypto-name">{name}</div>
+        </div>
+      </td>
+      <td className="col-3-content">{wallet.balance} {sym}</td>
+      <td className="col-4-content">{`$${balance.y}`}</td>
+    </tr>
+  );
+}
--- frontend/components/dashboard/wallets.jsx
+++ frontend/components/dashboard/wallets.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import React from 'react';
+import {WalletItem} from './wallet_item';
 
-export const Wallets = ({ balances, wallets }) => {
+export const Wallets = ({ balances, wallets, logos }) => {
+  console.dir(balances)
   return (
-    <div>
-      <div className="wallets-container">
-        <ul>
-          {Object.values(wallets).map(wallet => {
-            return (
-              <li>
-                
-              </li>
-            );
-          })}
-        </ul>
+    <div className="wallet-container">
+      <div>
+        <h2>Your Portfolio</h2>
+      </div>
+      <div className="wallet-content-container">
+        <div className="wallet-table-container">
+          <table className="wallet-table">
+            <tbody>
+              {Object.values(wallets).map((wallet, index) => (
+                <WalletItem
+                  key={index}
+                  id={index}
+                  wallet={wallet}
+                  balance={balances[index]}
+                  logo={logos[wallet.currency]}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="wallet-footer">
+          <span>Total Balance: </span>
+        </div>
       </div>
     </div>
-    );
+  );
   }
